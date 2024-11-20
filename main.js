35b21a2cc6968cd31030688e76dab0456e5685e9
tokoputkaradze <putkaradzetoko60@gmail.com>
2024-11-20 16:39:44 +0400

    done

---
+++ main.js
@@ -0,0 +1,16 @@
+let userName = "tornike putkaradze";
+let dayofWeek = "tuesday";
+let favoriteActivity = "reading";
+// console.log(userName, dayofWeek, favoriteActivity);
+console.log(
+  "Hi this is tornikes text",
+  userName,
+  "tornike",
+  dayofWeek,
+  "a perfect day",
+  favoriteActivity
+);
+
+let textUser =
+  "Hi my name is 'tornike putkaradze' Today is 'tuesday', a perfect day for 'reading'";
+console.log(textUser);
